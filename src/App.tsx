a29204f3f19bf57c147104c0d8295b810824a08d
tonnytech <tonnytei4@gmail.com>
2024-12-04 16:47:46 +0300

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
 
 function App() {
   return (
---
+++ src/pages/Store.tsx
@@ -0,0 +1,5 @@
+const Store = () => {
+  return <div>Home</div>;
+};
+
+export default Store;
